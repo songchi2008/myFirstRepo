883d3df652e1ae1294218ebf2784ead0c5dafb4a
songchi <songchi@quyiyuan.com>
2016-03-27 14:53:50 +0800

    why must comment

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
 	public statis void main(String[] args){
 		system.out.println("hello zhuting");
 		system.out.println("hello tianjie beauty");
+		system.out.println("55555555555555555555");
 	}
 }
