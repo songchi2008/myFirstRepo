8e9f20c8c9be0d4a1aacc03b9ff01012c7d982c3
songchi <songchi@quyiyuan.com>
2016-03-27 14:58:21 +0800

    12 insertions

---
+++ Wangwu.java
@@ -0,0 +1,12 @@
+public class Hello{
+	public statis void main(String[] args){
+		system.out.println("11111111111111");
+		system.out.println("2222222222222222222");
+		system.out.println("2222222222222222222");
+		system.out.println("2222222222222222222");
+		system.out.println("2222222222222222222");
+		system.out.println("2222222222222222222");
+		system.out.println("2222222222222222222");
+		system.out.println("2222222222222222222");
+	}
+}
