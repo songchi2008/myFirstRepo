189d4f5004516689853f33b3f842769cefc4c27d
songchi <songchi@quyiyuan.com>
2016-03-27 15:13:03 +0800

    delete lisi

--- lisi.java
+++
@@ -1,9 +0,0 @@
-public class Hello{
-	public statis void main(String[] args){
-		system.out.println("11111111111111");
-		system.out.println("2222222222222222222");
-		system.out.println("2222222222222222222");
-		system.out.println("2222222222222222222");
-		system.out.println("2222222222222222222");
-	}
-}
